b0744a4700bdb8c5fd113f95fa7fd565f7f6cc6b
Gene Foxwell <gene.foxwell@gmail.com>
2026-05-28 13:46:18 -0400

    (update): knowledge base generation

--- agentic_system.py
+++ agentic_system.py
@@ -1,19 +1,107 @@
+########################################################################
 # Interactive Fiction Agentic System
+########################################################################
+import json
+
+from dotenv import load_dotenv
+import os
+
+import argparse
+
 from pydantic_ai import Agent
 from pydantic import BaseModel, Field
+
+from pydantic_ai.providers.anthropic import AnthropicProvider
+from pydantic_ai.models.anthropic import AnthropicModel
+
+from mgraph_db.mgraph.MGraph import MGraph
+from mgraph_db.mgraph.schemas.Schema__MGraph__Node import Schema__MGraph__Node
+from mgraph_db.mgraph.schemas.Schema__MGraph__Node__Data import Schema__MGraph__Node__Data
+from mgraph_db.mgraph.schemas.Schema__MGraph__Edge import Schema__MGraph__Edge, Schema__MGraph__Edge__Data
+
+# Load environment variables from .env file
+load_dotenv()
+
+ANTHROPIC_API_KEY = os.getenv('ANTHROPIC_API_KEY')
+
+
+########################################################################
+# MGraph Objects
+########################################################################
+
+# Initialize the in-memory graph structure for storing the knowledge graph.
+mgraph = MGraph()
+
+# Custom Node for MGraph
+class Custom_Node_Data(Schema__MGraph__Node__Data):
+    name: str
+    type: str
+    description: str
+    
+class Custom_Node(Schema__MGraph__Node):
+    node_data: Custom_Node_Data
+
+
+# Custom Edge for MGraph
+class Custom_Edge_Data(Schema__MGraph__Edge__Data):
+    predicate: str = ""  # This allows the 'predicate' key inside edge_data
+
+class Custom_Edge(Schema__MGraph__Edge):
+    edge_data: Custom_Edge_Data
+
+########################################################################
+# Pydantic Objects
+########################################################################
 
 class PlotEntity(BaseModel):
     name: str = Field(description="Name of the entity extracted from the plot")
     type: str = Field(description="The type of entity")
     description: str = Field(description="Description of the extracted entity")
 
+class EntityCollection(BaseModel):
+    characters: list[PlotEntity] = Field(description="List of character entities extracted from the plot")
+    locations: list[PlotEntity] = Field(description="List of location entities extracted from the plot")
+    objects: list[PlotEntity] = Field(description="List of object entities extracted from the plot")
+    plot_events: list[PlotEntity] = Field(description="List of plot event entities extracted from the plot")
+    themes: list[PlotEntity] = Field(description="List of theme entities extracted from the plot")
+    genre: list[PlotEntity] = Field(description="List of genre entities extracted from the plot")
 
-# This system will work in two phases:
+class GraphTriplet(BaseModel):
+    subject: PlotEntity = Field(description="The subject of the relationship")
+    predicate: str = Field(description="The predicate describing the relationship")
+    object: PlotEntity = Field(description="The object of the relationship")
 
-# 1. Startup Phase
 
-# Load the plot file into context.
-def load_plot_file(filename="plot.md"):
+########################################################################
+#AGENTS
+########################################################################
+
+model = AnthropicModel(
+     'claude-haiku-4-5', provider=AnthropicProvider(api_key=ANTHROPIC_API_KEY)
+)
+
+entity_extraction_agent = Agent(
+    model,
+    output_type=list[PlotEntity],
+    system_prompt=("""You are an expert at extracting entities from a plot. You will be given a plot and a competency question. 
+                   Your task is to extract the relevant entities from the plot that answer the competency question. 
+                   Please provide your answer in a structured format, listing each entity with its name, type, and a brief description.""")
+) # type: ignore
+
+triplet_extraction_agent = Agent(
+    model,
+    output_type=list[GraphTriplet],
+    system_prompt=("""You are an expert at extracting relationships between entities in a plot. You will be given a plot and a list of entities extracted from the plot. 
+                   Your task is to identify the relationships between these entities based on the information provided in the plot. 
+                   Please provide your answer in a structured format, listing each relationship as a triplet with the subject, predicate, and object.""")
+) # type: ignore
+
+
+########################################################################
+# Startup/DB Generation Phase
+########################################################################
+
+def load_plot_file(filename:str="plot.md"):
     """
     loads the plot file from which the initial KG will be constructed.
 
@@ -22,32 +110,219 @@ def load_plot_file(filename="plot.md"):
     Returns:
         contents of the plot file.
     """
-    with open(filename, 'r') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         content = file.read()
         return content
 
 
+def extract_entities(plot: str, cq: str, entity_type: str) -> list[PlotEntity]:
+    """
+    Extracts entities from the plot based on the given competency question.
+
+    Args:
+        plot: The plot from which to extract entities.
+        cq: The competency question guiding the extraction process.
+        entity_type: The type of entities to extract.
+    """
+    prompt = f"""
+                Give each extracted Entity, the type: {entity_type}.
+
+                Competency Question:
+                {cq}
+                
+                Plot:
+                {plot}"""
+    
+    print(f"Extracting entities for CQ: {cq}")
+    return entity_extraction_agent.run_sync(prompt) # type: ignore
 
 
-# Load the Entity Extraction Competency Questions (CQ's)
-entity_extraction_cq = [
-    "Who are the characters of this story?", # Character Entities
-    "What locations are in this story?", # Location Entities
-    "What important objects are in this story?", # Object Entities
-    "What important plot points exist in this story?" # Plot Entities.
-]
+def load_entities_from_plot(plot: str) -> EntityCollection:
+    """
+    Loads the plot file and extracts entities based on predefined competency questions.
+    
+    Args:
+        plot: text of the plot
+    Returns:
+        An EntityCollection containing the extracted entities.
+    """
 
-# Execute the Entity Extraction CQ Prompt.
-# Combine Plot + Entity CQ Prompt Results into a new Context.
-# Execute the Relationship Creation Loop.
+    character_extraction_cq = "What are the main characters in the plot and their roles?"
+    location_extraction_cq = "What are the main locations in the plot and their significance?"
+    object_extraction_cq = "What are the key objects in the plot and their importance?" 
+    plot_event_extraction_cq = "What are the major events in the plot and their impact on the story?"
+    theme_extraction_cq = "What are the central themes in the plot and how are they represented?"
+    genre_extraction_cq = "What is the genre of the plot and what are its defining characteristics?"
 
-# 2. Interaction Phase
+    
+    character_entities_results = extract_entities(plot, character_extraction_cq, "Character")
+    location_entities_results = extract_entities(plot, location_extraction_cq, "Location")
+    object_entities_results = extract_entities(plot, object_extraction_cq, "Object")
+    plot_event_entities_results = extract_entities(plot, plot_event_extraction_cq, "PlotEvent")
+    theme_entities_results = extract_entities(plot, theme_extraction_cq, "Theme")
+    genre_entities_results = extract_entities(plot, genre_extraction_cq, "Genre")
 
-# Find starting location
-# Show user description.
+    return EntityCollection(
+        characters=character_entities_results.output,
+        locations=location_entities_results.output,
+        objects=object_entities_results.output,
+        plot_events=plot_event_entities_results.output,
+        themes=theme_entities_results.output,
+        genre=genre_entities_results.output
+    )  # type: ignore
 
-# Begin Agentic Loop
-#   User provides response.
-#   Agent loads relevants parts of the KG
-#   Updates KG as needed.
-#   Agent sends back response.
+
+def extract_relationships(plot: str, entities: list[PlotEntity]) -> list[GraphTriplet]:
+    """
+    Creates the basis of a Knowledge Graph by extracting relationships between entities based on the plot.
+
+    Args:
+        plot: The plot from which to extract relationships.
+        entities: The list of entities extracted from the plot to consider when identifying relationships.
+    Returns:        
+        list of GraphTriplet representing the relationships between entities in the plot.
+    """
+    prompt = f"""
+    Given the following plot and list of entities, identify the relationships between the entities based on the information provided in the plot.
+
+    Plot:
+    {plot}
+
+    Entities:
+    {entities}
+    """
+    print("Extracting relationships between entities...")
+    return triplet_extraction_agent.run_sync(prompt) # type: ignore
+
+
+
+def construct_knowledge_graph(relationships: list[GraphTriplet], output_file: str = "knowledge_graph.json"):
+    """
+    Constructs a knowledge graph from the extracted entities and relationships. This function can be implemented using a graph database or an in-memory graph structure depending on the requirements of the system.
+    Clears out the existing KG before initiating construction.
+    
+    Args:
+        relationships: The list of relationships between entities extracted from the plot.
+    """
+
+    # We build the KG from unique Entity names, so we need to keep track of which entities we've already added to the graph to avoid duplicates.
+    # We can use a dictionary to map entity names to their corresponding node IDs in the graph, which will allow us to easily reference existing nodes when adding relationships.
+    entities = {}
+
+    with mgraph.edit() as edit:
+        for triplet in relationships:
+            
+            subject_id = None
+            object_id = None
+
+            # insert base entites as nodes in the graph if they haven't already been added, and keep track of their node IDs in the entities dictionary
+            if triplet.subject.name in entities:
+                subject_id = entities[triplet.subject.name]
+            else:
+
+                subject = edit.new_node(
+                    node_type=Custom_Node, # type: ignore
+                    name=triplet.subject.name,
+                    type=triplet.subject.type,
+                    description=triplet.subject.description
+                )
+
+                subject_id = subject.node_id
+                entities[triplet.subject.name] = subject_id
+
+            if triplet.object.name in entities:
+                object_id = entities[triplet.object.name]
+            else:
+                object = edit.new_node(
+                    node_type=Custom_Node, # type: ignore
+                    name=triplet.object.name,
+                    type=triplet.object.type,
+                    description=triplet.object.description
+                )
+                object_id = object.node_id
+                entities[triplet.object.name] = object_id
+            
+            # insert relationship as an edge in the graph, referencing the node IDs of the subject and object
+            edit.new_edge(
+                edge_type=Custom_Edge,      # Tells mgraph to use your new schema
+                from_node_id=subject_id,
+                to_node_id=object_id,
+                edge_data={
+                    "predicate": triplet.predicate  # This will now bypass the type-checker!
+                }
+            )
+
+    # save the graph
+    print(f"Saving knowledge graph to {output_file}...")
+    with mgraph.export() as export:
+        data = export.to__mgraph_json()
+        with open(output_file, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+
+def get_input_args():
+    """
+    Retrieves and parses the command-line arguments.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--plot_file', type=str, default='plot.md', help='Path to the plot file')
+    parser.add_argument('--construct_graph', action='store_true', help='Whether to construct the knowledge graph')
+    parser.add_argument('--load_graph', action='store_true', help='Whether to load an existing knowledge graph')
+    parser.add_argument('--graph_file', type=str, default='knowledge_graph.json', help='Path to the knowledge graph file (if loading an existing graph)')
+
+    return parser.parse_args()
+
+
+def graph_construction_pipeline(plot_file: str):
+    """
+    Constructs a knowledge graph from the given plot file by extracting entities and relationships.
+    The KG only needs to be constructed once per plot, and can be updated as needed during the interaction phase.
+
+    Args:
+        plot_file: The path to the plot file from which to construct the knowledge graph.
+    """
+    plot = load_plot_file(filename=plot_file)
+    entity_collection = load_entities_from_plot(plot)
+    relationships = extract_relationships(plot, entity_collection.characters + entity_collection.locations + entity_collection.objects + entity_collection.plot_events + entity_collection.themes + entity_collection.genre)
+    construct_knowledge_graph(relationships.output)
+
+
+########################################################################
+# Interactive/RAG Phase
+########################################################################
+
+def load_knowledge_graph(graph_file: str):
+    """
+    Loads an existing knowledge graph from a file. This can be used to load a previously constructed KG without needing to reconstruct it from the plot.
+
+    Args:
+        graph_file: The path to the knowledge graph file to load.
+    """
+    print(f"Loading knowledge graph from {graph_file}...")
+    data = None
+    with open(graph_file, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    mgraph.from_json(data)
+
+
+
+
+########################################################################
+# Main Entrypoint
+########################################################################
+
+def main():
+    command_line_args = get_input_args()
+
+    # Phase 1: Parse the Plot into a KG.
+    if command_line_args.construct_graph:
+        graph_construction_pipeline(command_line_args.plot_file)
+    else:
+        load_knowledge_graph(command_line_args.graph_file)    
+
+    # Phase 2: Interact with the user using the KG.
+    
+
+
+if __name__ == "__main__":
+    main()
--- plot.md
+++ plot.md
@@ -1,1 +1,120 @@
-# SOmething happens here
+# Zania's Locket
+“Your shift will begin in the next five minutes. Please ensure that you are fully prepared to perform your duties. Remember! An efficient hospice is a happy hospice!”, Zania Sagan’s watch lit up with the daily five-minute warning timer as she quickly downed her morning coffee and made her way over the teleportation stations to sign in for the days’ work.
+
+“Thank you for confirming your service!”, the machine chimed as she pressed her palm onto the identification panel. “We have assigned you to the cleaning service for today’s work cycle. Your first assignment will be room number 1142. Teleportation will begin in four minutes and thirteen seconds. Enjoy your work day!”
+
+Zania sighed as her hand instinctively reached for the small, heart-shaped locket around her neck. She rubbed it nervously for a few moments, noting anyone nearby who might watch her, then lifted the golden locket up to her mouth. “Samuel? You hear me Samuel?”
+
+“You need to stop calling - you’ll draw attention to yourself!”, a gruff voice crackled over the intercom.
+
+“I know, I know, but I just checked in, central computer is teleporting me to room 1142 in about ... three minutes. Can we redirect the teleport directly to the processing center from there?”
+
+“Not a chance. There hasn’t been a death around there for ages - if I redirect you to the processing center you’ll have security drones on your butt faster than I can get you out.”, there was a momentary pause as Zania heard echoes of Samuel’s heavy paws working their way around his oversized keyboard, “What we need is a distraction. Think you can magic one up?”
+
+Zania glanced at her watch as she nervously rubbed the locket. One minute, fifteen seconds until the central computer teleported her to her first assignment. “I don’t really have much choice, do I?”
+
+“Only if you plan on giving up right here and now. Will you be ok to go through with this?”
+
+Zania took a deep breath to clear her head, glancing back at the timer - fifteen seconds left - “I got this!”
+
+Her eyes burned as the world filled with an unbearably bright white light, followed immediately by darkness. “Teleportation in progress, you will have ten minutes, thirty seconds to complete this cleaning task. Failure to complete your task on time will cause a reprimand on your record.”
+
+The world materialized before her eyes. She was now standing in a small dorm room with dark wooden paneling. In front of her was a yellow bucket full of various tools and supplies. To her left a small faux-mahogany dresser stood beside a cracked leather chair on one side and a large green mechanical bed was standing on the right. Laying on the bed appeared to be mummified remains of the room previous occupant, rendered almost unrecognizable by the vast array of clear plastic tubing protruding outward from its various orifices. A high-pitched ding rang through the room, followed by a now familiar voice - “Please begin specimen clean-up procedures. You have ten minutes remaining to complete this task.”
+
+“Ok”, Zania muttered to herself. “Distractions ... distraction, how can I ...”, she turned her attention to the tube filled corpse laying on the bed and lifted her locked up to her mouth. “Samuel - I think I have an idea, but I need your help.”
+
+“I’ll do my best - but no promises. What are you thinking?”
+
+“Can we make this locket thing act like a battery?”
+
+“Yeah, but why?”
+
+“Just do it!” Zania grabbed a sharp trowel from the bucket and climbed up on the bed, straddling herself overtop the corpses. Bracing herself, Zania raised the trowel above her head and plunged into the dead man’s chest. There was a moment of doubt as the trowel sliced into the mummified flesh, followed by a satisfied sense of relief as the trowel bashed up against the cybernetic implants that had extended man’s life far beyond what was natural. She twisted the trowel, separating the dried flesh from the device, exposing a small indentation. Zania quickly shoved the locket inside and rushed to the opposite end of the room.
+
+Zania covered her ears as an ear-piercing screech filled the room. On the bed, the corpse’s eyes shot open as the creature pulled itself into an upright position. A voice echoed over the intercom, struggling to compete with the high-pitched sound emanating from the zombie like monster - “we have detected a cybernetic malfunction on floor 74 room 1142. Please complete your current task and evacuate in an orderly matter”.
+
+Zania pried the locket from the zombie’s chest, stuffed it into her pocket, and threw open the hallway door. Workers rushed out of the surrounding doors, scrambling towards the teleport chamber at the end of the hall. As she pushed through the crowd, Zania passed by several workers that simply evaporated into a wisp of light.
+
+“Reminder!” the voice echoed through the hall. “Please complete your task before evacuating - work is our priority!”
+
+“Zania!”, the voice in her pocket hissed. “What in hell did you do? The entire facility has gone nuts!”
+
+Zania ignored Samuel’s rantings and elbowed her way onto the teleportation platform. “Central Computer Core”, she commanded.
+
+“Central Computer Core is a restricted area, authorization required,” the computer answered back.
+
+Zania fetched the locket from her pocket, opened clasp and held it up against the pad on the side of the teleportation chamber. A green light blinked, followed by a far more polite notice by the computer showing her request had succeeded. She clamped her eyes shut and waited for the world around her to melt into a flash of light.
+
+When her eyes opened again, Zania found herself in a deserted auditorium, the empty seats watching her every move. At the center of the auditorium, a large cylindrical pedestal raised from the floor, decorated by blinking lights, charts, and indicators whose purpose she couldn’t comprehend.
+
+“Zania! Are you listening to me? ZANIA!”, Zania could barely make out Samuel’s muffled voice from the locket in her tightly clenched hand.
+
+“You sent me to the wrong place!” Zania barked into the locket. “How long until you can redirect this thing to the computer core?”
+
+“System says you are right beside the central computer core - nothing else I can do about that,” Samuel responded.
+
+“But this can’t be - it just can’t. I’m in an auditorium, not a server room.”
+
+“Are you my new friend?” a childlike voice echoed from the pedestal at the center of the room.
+
+“Who said that?” Zania spun around, searching for the source of the voice.
+
+“Um... I did?!”, the voice was coming from the center of the room this time, Zania was sure of it.
+
+“Who are you? Where are you?”, Zania called out.
+
+“I’m Sasha - are you Zania? The voice says that your name. Can you help me, Zania?”, the sound was definitely coming from the pedestal.
+
+“Maybe? What’s wrong? If I help you, can you take me to the central computer core?”
+
+“There’s too much work! I just want to play, but Grandmother says I have to finish all my work before I can have any fun! Could you do it for me?”, the voice pleaded.
+
+“What work? What do you need?”
+
+“I have to make everything is in its place - but its just too much ... I can’t to do it - I won’t!”, a green tinted translucent representation of the Hospice appeared before Zania’s eyes. It was lit up in hundreds of places by pinpoints of white light that seemed to be around the halls. At its center, Zania saw the large auditorium, mostly empty, with only a single light shinning in the center. “You aren’t in your place either! Why can’t people stay in their place - it’s not fair!”
+
+“The computer core, whatever it is, I think its inside that pedestal,” Samuel’s voice emanated from the locket, “if you can get the locket up close, I think I can open it up - but the rest, well that’s up to you.”
+
+“Who was that? Whose with you? They don’t belong here, you know! I’ll have to get rid of them too!”
+
+“What do you mean, get rid of them? What happens to the people who don’t belong?”, Zania stepped back from the pedestal, clutching the locket for reassurance.
+
+“They go to the soup, of course!”, the voice ended its sentence with a series of eerie giggles that echoed throughout the auditorium.
+
+“Get the locket closer to the pedestal before it realizes you are the one out of place!”, Samuel insisted.
+
+Zania shook her head and ran over to the pedestal holding the locket out in front of her, the locket beeping in a rhythmic pattern as she got closer. A loud hiss reverberated through the auditorium as the locket came within a foot of the pedestal. Slowly the metal casing of the device slid open, then retracted smoothly into the floor, revealing the creature within. Floating inside a vial of blueish liquid was a small, child sized brain, it’s pinkish hue just barely visible through the fluids that sustained it. Plastic tubing pumped the liquid in and out of the vial, ensuring that the liquid always kept fresh.
+
+“What the hell are you?”, Zania exclaimed.
+
+“Grandmother says I am just like her now!”, there was a hint of glee in the creature’s voice. “she says I have a very important job taking care of all the rich people.”
+
+“You’re the central computer core aren’t you?”
+
+“Ah huh - didn’t you know that? This is where the grownups come to talk to me! No one’s visited me in such a long time. It was too much! I couldn’t do it! I just wanted to play!” Zania was certain she could hear the creature sobbing as it spoke.
+
+Zania felt a chill in her spine as she realized just how many empty seats the auditorium really had. “Why haven’t they come? Where did the grownups go?”
+
+“To the soup, of course! Grandmother said they wanted to hurt me, so I added them to the soup! They taste great too!”, the tubes that ran from the creature’s vial seemed to shake with delight.
+
+“What soup?”, Zania asked, hoping she was wrong about the answer.
+
+“The one the old people get put into, of course! I keep them happy in their last days, and in return they give themselves to the soup. Grandmother says that’s the best thing to happen to anyone!”, the creature giggled again.
+
+“And the people who were out of place? Are they?”, Zania felt the hair raise on the back of her neck.
+
+“Right into the soup - just like Grandmother would’ve wanted! Actually makes the soup even better!”, the creature shook its tubes gleefully.
+
+“Zania”, Samuels voice was stern, “this has to stop.”
+
+“I know, but how do I stop it?”
+
+“Break the glass!”
+
+“Won’t that kill the child?”
+
+“It’s not a child anymore, if it ever was. This has to be done!”
+
+Zania took a deep breath and examined the pedestal again. If there was a way to disconnect the brain safely from its pedestal, she didn’t see it. Still, regardless of what the creature had done, it was still a child. She’d signed up to destroy a computer system - strike a blow against the corruption that Lantern Industries inflicted on the galaxy.
+
+She had to make a choice. Save the child, or save the galaxy. Her hands shook, her heart raced. She stared at the horrible creature through streams of tears. Shaking in anger, Zania stepped forward and made her choice.
